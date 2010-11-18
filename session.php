36dccc8444d0d32d60c55c648e88737747d5c142
James Nylen <jnylen@gmail.com>
2010-11-17 22:42:07 -0500

    Remove broken session check

--- session.php
+++ session.php
@@ -1,10 +1,6 @@
 <?php
 function rtgui_session_start() {
   if(!$_SESSION) {
-    session_set_cookie_params(
-      60 * 60 * 6, # 6 hours
-      str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname(__FILE__))
-    );
     session_start();
   }
 }
