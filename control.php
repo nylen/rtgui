nylen/rtgui

commit 33eba8e01455dc2c1a8aa7f71120fb606e389306

Don't send unnecessary torrent fields to client side

functions.php
@@ -122,7 +122,7 @@ function do_xmlrpc($request) {
 }
 
 // Get full list - retrieve full list of torrents
-function get_all_torrents($torrents_only=false, $view='main') {
+function get_all_torrents($torrents_only=false, $for_script=false, $view='main') {
   global $download_dir, $use_groups;
   global $tracker_highlight, $tracker_highlight_default;
 
@@ -287,6 +287,23 @@ function get_all_torrents($torrents_only=false, $view='main') {
     unset($t['connection_current']);
     unset($t['hashing']);
     unset($t['size_chunks']);
+
+    if($for_script) {
+      // unset items that aren't needed by the client
+      unset($t['directory']);
+      unset($t['is_active']);
+      unset($t['is_hash_checked']);
+      unset($t['is_hash_checking']);
+      unset($t['is_multi_file']);
+      unset($t['is_open']);
+      unset($t['is_private']);
+      unset($t['peers_complete']);
+      unset($t['peers_connected']);
+      unset($t['peers_not_connected']);
+      unset($t['priority']);
+      unset($t['state_changed']);
+      unset($t['tied_to_file']);
+    }
 
     $torrents[$hash] = $t;
   }

index.php
@@ -36,7 +36,7 @@ if(!isset($r_debug)) {
 $_SESSION['persistent'] = array();
 
 // Get the list of torrents downloading
-$data = get_all_torrents();
+$data = get_all_torrents(false, true);
 
 // Turn it into JSON and format it somewhat nicely
 $data_str = json_encode($data);

json.php
@@ -20,7 +20,7 @@ require_once 'config.php';
 require_once 'functions.php';
 rtgui_session_start();
 
-$data = get_all_torrents(false);
+$data = get_all_torrents(false, true);
 
 if(@is_array($_SESSION['last_data'])) {
   $last_data = $_SESSION['last_data'];

rssfeed.php
@@ -46,7 +46,7 @@ echo "<generator>rtGui - http://rtgui.googlecode.com/ </generator>";
 echo "<link>".get_rtgui_url()."</link>";
 echo "<lastBuildDate>".date("r")."</lastBuildDate>";
 
-$data = get_all_torrents(true, $r_view);
+$data = get_all_torrents(true, false, $r_view);
 
 if (is_array($data)) {
    $sortkey="state_changed";

commit 2489bd835aa8f3f37465a2bda4c362473bafe438

Fix problems introduced by commit 33eba8e01455dc2c1a8aa7f71120fb606e389306

control.php
@@ -117,9 +117,6 @@ switch($r_cmd) {
 }
 
 if(!$r_ajax) {
-  rtgui_session_start();
-  $_SESSION['must_get_all'] = true;
-  session_write_close();
   $hash = ($r_tab ? "#$r_tab" : '');
   @header('Location: ' . $_SERVER['HTTP_REFERER'] . $hash);
 }

functions.php
@@ -551,7 +551,7 @@ function scgi_send($host, $port, $data, $timeout=5) {
 }
 
 
-// ---------- Old functions that should probably go away one day
+// ---------- Old functions
 
 // multibyte-safe replacement for wordwrap.
 // (See http://code.google.com/p/rtgui/issues/detail?id=71 - Thanks llamaX)

view.php
@@ -18,7 +18,7 @@
 
 require_once 'config.php';
 require_once 'functions.php';
-rtgui_session_start();
+
 import_request_variables('gp', 'r_');
 
 if(isset($r_select)) {
@@ -51,13 +51,7 @@ include_script('view.js');
 
 
 // Get torrent info...  (get all downloads, then filter out just this one by the hash)
-if(is_array($_SESSION['last_data']) && !$_SESSION['must_get_all']) {
-  $all_torrents = $_SESSION['last_data']['torrents'];
-} else {
-  $_SESSION['must_get_all'] = false;
-  $all_torrents = get_all_torrents(true);
-}
-session_write_close();
+$all_torrents = get_all_torrents(true);
 
 $this_torrent = false;
 foreach($all_torrents as $torrent) {