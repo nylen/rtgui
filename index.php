7a705e92cbf8c4981cf9c7ca1232e97d0e3ab46a
James Nylen <jnylen@gmail.com>
2012-07-08 20:12:03 -0400

    Include underscore.js and backbone.js

--- index.php
+++ index.php
@@ -123,6 +123,8 @@ include_script('jquery-ui-1.8.9-files/js/jquery-ui-1.8.9.custom.min.js');
 include_script('jquery.cookie.js');
 include_script('jquery.jeegoocontext.js');
 include_script('jquery.mousewheel.js');
+include_script('underscore.js');
+include_script('backbone.js');
 include_script('swig.js');
 include_script('json2.min.js');
 include_script('php.min.js');
